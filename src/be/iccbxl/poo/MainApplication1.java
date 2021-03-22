a9d45806f88364a67a49b8a847e2e8160d6b2b05
Owandji <owandji@hotmail.com>
2021-03-22 17:18:15 +0100

    Déclaration des tableaux

---
+++ src/be/iccbxl/poo/MainApplication1.java
@@ -0,0 +1,13 @@
+package be.iccbxl.poo;
+
+public class MainApplication1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] membres =();
+		String[] livres = ();
+		Int[] emprunts = ();
+
+	}
+
+}
