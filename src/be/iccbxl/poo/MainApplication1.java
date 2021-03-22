4d63bbbc1f491688e485000b876b67b5b4cc7156
Owandji <owandji@hotmail.com>
2021-03-22 17:26:05 +0100

    Affichage du message de bienvenu

--- src/be/iccbxl/poo/MainApplication1.java
+++ src/be/iccbxl/poo/MainApplication1.java
@@ -4,9 +4,11 @@ public class MainApplication1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] membres =();
-		String[] livres = ();
-		Int[] emprunts = ();
+		String[] membres = {};
+		String[] livres = {};
+		int[] emprunts = {};
+		
+		System.out.println("Bonjour, bienvenu de ce cours de programmation");
 
 	}
 
