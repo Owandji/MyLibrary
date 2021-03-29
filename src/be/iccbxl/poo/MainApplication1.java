3d010d9c97deade3c0990fe846c962ad9846544b
Owandji <owandji@hotmail.com>
2021-03-29 16:11:10 +0200

    Affichage du menu Library

--- src/be/iccbxl/poo/MainApplication1.java
+++ src/be/iccbxl/poo/MainApplication1.java
@@ -1,15 +1,49 @@
 package be.iccbxl.poo;
 
+import java.util.Scanner;
+
 public class MainApplication1 {
+	static Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] membres = {};
-		String[] livres = {};
-		int[] emprunts = {};
+			
+		System.out.println("Bienvenue à la librarie Les AS");
+		System.out.println("------------------------------");
 		
-		System.out.println("Bonjour, bienvenu de ce cours de programmation");
+	int cmd = 0;
+			
+			do {
+				// Affichage du menu principal 
+					System.out.println("1 - Créer un membre\n" 
+					   +"2 - Créer un livre\n"
+					   +"3 - Emprunter un livre\n"
+					   +"4 - Afficher les statistiques\n"
+					   +"0 - Quitter\n");
+				
+				//Lire le choix de l'utilisateur
+				System.out.println("Entrez votre choix: ");	
+				cmd = s.nextInt();
+				
+				//Traiter la demande de l'utilisateur 
+				switch(cmd) {
+				case 1:
+					System.out.println("Ajouter un membre");
+					break;
+				case 2:
+					System.out.println("Ajouter un livre");
+					break;
+				case 3:
+					System.out.println("Emprunter un livre");
+					break;
+				case 4:
+					System.out.println("Afficher les statistiques");
+					break;
+				}
+			
+				
+			} while(cmd!=0);
+			
 
-	}
+		}
 
 }
